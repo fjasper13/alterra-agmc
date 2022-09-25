7bcf4ce381bbdd0780227776e213cfcaeccdb182
fjasper13 <fulgentiusjasper@gmail.com>
2022-09-25 15:48:45 +0700

    update yml and main.go

--- day-10/main.go
+++ day-10/main.go
@@ -5,18 +5,16 @@ import (
 	"day-10/internal/factory"
 	"day-10/internal/http"
 	m "day-10/internal/middlewares"
-	"errors"
 	"os"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	// Check .env file
-	if err := godotenv.Load(); err != nil {
-		panic(errors.New("Error loading .env file"))
-	}
+	// if err := godotenv.Load(); err != nil {
+	// 	panic(errors.New("Error loading .env file"))
+	// }
 	jwt := os.Getenv("SECRET_JWT")
 	if jwt == "" {
 		panic("SECRET JWT is required")
